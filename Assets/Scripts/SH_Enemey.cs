ba4a5b120eeaa44f729d3062dd1598f0d2a8b2fc
thomas byrne <thomas.jo.byrne@gmail.com>
2015-11-16 14:42:03 +0000

    enemy movement(basic)

---
+++ Assets/Scripts/SH_Enemey.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SH_Enemey : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        //transform.position = ( SH_GameManager.GM.ViableTargets[0].transform.position - transform.position) * (Speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, SH_GameManager.GM.ViableTargets[0].transform.position, Speed * Time.deltaTime);
+	
+	}
+
+    public float Speed = 0.5f;
+}
---
+++ Assets/Scripts/SH_GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SH_GameManager : MonoBehaviour {
+
+
+    public List<GameObject> ViableTargets;
+
+    public static SH_GameManager GM;
+
+    void Awake()
+    {
+        if (GM == null)
+        {
+            GM = this;
+        }
+
+    }
+    
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
