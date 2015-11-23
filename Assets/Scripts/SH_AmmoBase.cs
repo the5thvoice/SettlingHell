c18dc75dda25cbc1067d335ccebf7955bb1624d9
tanya byrne <tanya.jo.byrne@gmail.com>
2015-11-23 19:23:28 +0000

    class end( started projectile)

---
+++ Assets/Scripts/SH_AmmoBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SH_AmmoBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/SH_BattleTower.cs
+++ Assets/Scripts/SH_BattleTower.cs
@@ -5,6 +5,7 @@ using System;
 public class SH_BattleTower : SH_Building {
 
     public GameObject TargetEnemy;
+    public GameObject Ammotype;
     public float speed = 6f;
 
     public override void Update()
@@ -15,15 +16,29 @@ public class SH_BattleTower : SH_Building {
 
     }
 
+
+    /// <summary>
+    /// rotates to and tracks current target 
+    /// </summary>
     private void TrackTarget()
     {
         transform.LookAt(transform.position + new Vector3(0, 0, 1), TargetEnemy.transform.position - transform.position);
         
 
     }
-        
-        
 
+    /// <summary>
+    /// fires of a pojectile
+    /// </summary>
+    private void Shoot()
+    {
+        return;
+    }
+        
+        
+    /// <summary>
+    /// find the target that is closest to tower
+    /// </summary>
     private void FindTarget()
     {
         return;
