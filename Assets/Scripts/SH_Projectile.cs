215ba4082adcf2fbff281042c7d378db5ceb7555
tanya byrne <tanya.jo.byrne@gmail.com>
2015-12-07 13:57:08 +0000

    projectileMovement

---
+++ Assets/Scripts/SH_Projectile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SH_Projectile : MonoBehaviour
+{
+
+    public float Speed = 20;
+    public bool OnScreen;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OnScreen)
+            transform.Translate(Vector2.up * Speed * Time.deltaTime);
+
+    }
+
+    public void OnBecameInvisible()
+    {
+
+        OnScreen = false;
+        
+
+
+    }
+}
