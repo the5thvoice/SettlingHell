3e05eb7e7da878a84099d1735255439d03b36a45
thomas byrne <thomas.jo.byrne@gmail.com>
2015-12-16 13:09:49 +0000

    MenuTransitions And Intro

---
+++ Assets/Scripts/IntroductionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroductionScript : MonoBehaviour {
+
+	float timer = 5;
+	// Update is called once per frame
+	void Update () {
+
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            return;
+        }
+
+        gameObject.SetActive(false);
+
+
+	
+	}
+}
---
+++ Assets/Scripts/SH_MenuTransitions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SH_MenuTransitions : MonoBehaviour {
+
+    /// <summary>
+    /// quits to main menu
+    /// </summary>
+    public void QuitToMain()
+    {
+        Application.LoadLevel(0);
+    }
+
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
